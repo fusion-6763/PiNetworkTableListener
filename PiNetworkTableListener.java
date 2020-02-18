53bb2fb9e23edefcb5cbc450c8bc7ec2503c6d82
Matt Coughlin <coughlin26@gmail.com>
2020-02-17 20:27:47 -0500

    Adding the listener

---
+++ PiNetworkTableListener.java
@@ -0,0 +1,35 @@
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+class PiNetworkTableListener {
+  private NetworkTableInstance ntInst;
+
+  public PiNetworkTableListener() {
+    ntInst = NetworkTableInstance.getDefault();
+    ntInst.setClientTeam(6763);
+  }
+
+  public static void main(String[] args) {
+    final PiNetworkTableListener piNetworkTableListener = new PiNetworkTableListener();
+    piNetworkTableListener.run();
+  }
+
+  public void run() {
+    final NetworkTable table = ntInst.getTable("pi-table");
+    final NetworkTableEntry processingOnEntry = table.getEntry("on");
+    ntInst.startDSClient(); // recommended if running on DS computer; this gets the robot IP from the DS
+    while (true) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException ex) {
+        System.out.println("interrupted");
+        return;
+      }
+      boolean on = processingOnEntry.getBoolean(false);
+      System.out.println("Vision Processing Enabled: " + on);
+      // TODO: Toggle the chameleon vision command here.
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PiNetworkTableListener
 A NetworkTable client for our raspberry pi.
+
+The Roborio updates a NetworkTable with a boolean. True starts the Chameleon Vision processing program, False stops it.
